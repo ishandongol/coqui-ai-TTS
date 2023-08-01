866a2793f68e1329f665591a57e75a3cf660224f
Ishan Dongol <ishandongol@gmail.com>
2023-08-01 19:45:49 +0545

    Custom Dataset

--- TTS/tts/datasets/formatters.py
+++ TTS/tts/datasets/formatters.py
@@ -188,6 +188,24 @@ def openslr(root_path, manifest_file, **kwargs):  # pylint: disable=unused-argum
     return items
 
 
+def custom_nepali(root_path, manifest_file, **kwargs):  # pylint: disable=unused-argument
+    """Assumes each line as ```<filename>|<transcription>|<transcription>```
+    """
+    txt_file = os.path.join(root_path, manifest_file)
+    items = []
+    speaker_name= "custom_nepali"
+    with open(txt_file, "r", encoding="utf-8") as ttf:
+        for line in ttf:
+            cols = line.split("|")
+            wav_file = os.path.join(root_path, "wavs", cols[0]+".wav")
+            text = cols[2]
+            # speaker_name = cols[0].split("_")[1]
+            items.append({"text": text, "audio_file": wav_file,
+                         "speaker_name": speaker_name, "root_path": root_path})
+    return items
+
+
+
 def ljspeech_test(root_path, meta_file, **kwargs):  # pylint: disable=unused-argument
     """Normalizes the LJSpeech meta data file for TTS testing
     https://keithito.com/LJ-Speech-Dataset/"""
